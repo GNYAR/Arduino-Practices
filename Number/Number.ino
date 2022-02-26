582d735676ae7b16b8cc3b2b3eb8689b6879ace6
GNYAR <s901121s901121@gmail.com>
2022-02-26 21:07:37 +0800

    Number carousel (0-9)

---
+++ Number/Number.ino
@@ -0,0 +1,55 @@
+/*
+  本實作顯示器為共陽極
+
+        接線  亮   暗
+  共陽:  5V  LOW  HIGH
+  共陰: GND  HIGH LOW
+*/
+
+int DOT_PIN = 2;
+int NUMBERS[10][7] = {
+    {1, 1, 1, 1, 1, 1, 0},
+    {0, 1, 1, 0, 0, 0, 0},
+    {1, 1, 0, 1, 1, 0, 1},
+    {1, 1, 1, 1, 0, 0, 1},
+    {0, 1, 1, 0, 0, 1, 1},
+    {1, 0, 1, 1, 0, 1, 1},
+    {1, 0, 1, 1, 1, 1, 1},
+    {1, 1, 1, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 0, 1, 1}};
+
+void showNum(int x)
+{
+  int start = DOT_PIN + 1;
+  for (int i = 0; i < 7; i++)
+    digitalWrite(start + i, !NUMBERS[x][i]);
+}
+
+void clear()
+{
+  for (int i = DOT_PIN; i < DOT_PIN + 8; i++)
+  {
+    digitalWrite(i, HIGH);
+  }
+}
+
+void setup()
+{
+  for (int i = DOT_PIN; i < DOT_PIN + 8; i++)
+  {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);
+    delay(500);
+  }
+  clear();
+}
+
+int CNT = 0;
+void loop()
+{
+  if (CNT == 10)
+    CNT = 0;
+  showNum(CNT++);
+  delay(3000);
+}
